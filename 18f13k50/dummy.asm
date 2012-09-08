69aaf2ad02835b37cbe049247f3ab841580f5eec
Holger Oehm <holger.oehm@holger-oehm.de>
2012-09-09 01:39:41 +0200

    add dummy main routine to boot loader hex file
    so that after flashing the PIC with the boot loader the first time
    the PIC boots, it immediately invokes the boot loader

---
+++ 18f13k50/dummy.asm
@@ -0,0 +1,27 @@
+; USB bootloader for PICs
+; dummy vector definition, to allow devices without jumpers to flash once
+; Copyright (C) 2012 Holger Oehm
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
+#include <p18f13k50.inc>
+
+;**************************************************************
+; vectors
+resetvector		ORG	0x0800
+	goto	0x001c		; setEEmark in the bootloader
+hiprio_interruptvector	ORG	0x0808
+	goto	$
+lowprio_interruptvector	ORG	0x0818
+	goto	$
+			END
