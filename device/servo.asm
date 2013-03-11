0591ff03823951a524b15fb08a105fb0b75510ab
Holger Oehm <holger.oehm@holger-oehm.de>
2013-03-11 18:44:45 +0100

    implement 5 independent servo pulses

--- device/servo.asm
+++ device/servo.asm
@@ -33,10 +33,21 @@
 
 ; local definitions
 servo_udata		UDATA
-position		RES	5
+position		RES 5
+loopCounter		RES 1
+positionCounter		RES 5
 			UDATA_OVR
-waitLoopCntH		RES 1
 waitLoopCntL		RES 1
+
+; local macros
+compareServoPos	macro	index
+	movf	positionCounter+index, F, BANKED
+	bz	servo#v(index)isDone
+	decf	positionCounter+index, F, BANKED
+	skpnz
+servo#v(index)isDone
+	bcf	LATC, 3+index, ACCESS
+		endm
 
 ;**************************************************************
 ; Code section
@@ -45,28 +56,51 @@ waitLoopCntL		RES 1
 initServo
 	banksel	position
 	movlw	0x80
-	movwf	position, BANKED
+	movwf	position+0, BANKED
+	movwf	position+1, BANKED
+	movwf	position+2, BANKED
+	movwf	position+3, BANKED
+	movwf	position+4, BANKED
 	clrf	LATC, ACCESS
 	movlw	b'00000111'	; set RC3:7 to output
 	movwf	TRISC, ACCESS
 	return
 
 sendServoImpulse
-	movlw	b'00001000'
+	banksel	position
+	call	setServoAddress
+	movf	POSTINC0, W, ACCESS
+	movwf	positionCounter+0, BANKED
+	movf	POSTINC0, W, ACCESS
+	movwf	positionCounter+1, BANKED
+	movf	POSTINC0, W, ACCESS
+	movwf	positionCounter+2, BANKED
+	movf	POSTINC0, W, ACCESS
+	movwf	positionCounter+3, BANKED
+	movf	POSTINC0, W, ACCESS
+	movwf	positionCounter+4, BANKED
+	movlw	0xff
+	movwf	loopCounter, BANKED
+	movlw	b'11111000'
 	movwf	LATC, ACCESS
 	movlw	0x01
 	call	waitMilliSeconds
-	banksel	position
-	movf	position, W
-	bz	zeroPos
+
+outerLoop
+	compareServoPos	0
+	compareServoPos	1
+	compareServoPos	2
+	compareServoPos	3
+	compareServoPos	4
+
 	call	wait256thMs
+	banksel	position
+	decfsz	loopCounter, BANKED
+	goto	outerLoop
+
 zeroPos
 	movlw	b'00000000'
 	movwf	LATC, ACCESS
-	banksel	position
-	movf	position, W
-	sublw	0x00
-	call	wait256thMs
 	return
 
 setServoAddress
@@ -77,16 +111,12 @@ setServoAddress
 	return
 
 wait256thMs				;	2	-	2
-	banksel	waitLoopCntH		;	1	-	3
-	movwf	waitLoopCntH, BANKED	;	1	-	4
-outerLoop
-	movlw	D'28'			;	1	-	5
-	movwf	waitLoopCntL, BANKED	;	1	-	6
+	banksel	waitLoopCntL
+	movlw	D'20'			;	1	-	3
+	movwf	waitLoopCntL, BANKED	;	1	-	4
 innerLoop
-	decfsz	waitLoopCntL, BANKED	;	1	13	19,47
-	goto	innerLoop		;	2	26	45
-	decfsz	waitLoopCntH, BANKED	;	1	-	48
-	goto	outerLoop		;	2	-	48
-	return				;	2	-	50
+	decfsz	waitLoopCntL, BANKED	;	1	13	17,45
+	goto	innerLoop		;	2	26	43
+	return				;	2	-	48
 
 	END
